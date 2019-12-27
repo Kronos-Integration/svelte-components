aa365de9d778fd2c6cf01482735982925f5f34bd
Markus Felten <Markus.Felten@gmx.de>
2019-12-27 20:59:37 +0100

    wip

---
+++ src/index.mjs
@@ -0,0 +1,43 @@
+
+  export function endpointFor(services, exp) {
+    const m = exp.match(/service\((\w+)\)\.(.+)/);
+
+    if (m) {
+      return services[m[1]].endpoints[m[2]];
+    }
+  }
+
+export function normalizeServices(services) {
+  let cx = 110;
+  let y = 0;
+  for (const service of Object.values(services)) {
+    service.x = 10;
+    service.y = y;
+
+    let ey = 10 + 20 + 5;
+    for (const [name, endpoint] of Object.entries(service.endpoints)) {
+      endpoint.name = name;
+      endpoint.service = service;
+      endpoint.x = sw - 10;
+      endpoint.y = ey;
+      if (endpoint.connected === undefined) {
+        endpoint.connected = [];
+      } else if (!Array.isArray(endpoint.connected)) {
+        endpoint.connected = [endpoint.connected];
+      }
+      endpoint.connected = endpoint.connected.map(c => {
+        cx = cx + 5;
+
+        return { x: cx, target: c };
+      });
+      ey += 12;
+    }
+
+    service.w = sw;
+    service.h = ey > sh ? ey : sh;
+
+    y += service.h + 10;
+  }
+
+  return services;
+}
