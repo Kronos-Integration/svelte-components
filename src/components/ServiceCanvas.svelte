1f89176f90a9dde83c9ef39bf55a0ad2b3da1342
Markus Felten <Markus.Felten@gmx.de>
2020-09-02 21:25:40 +0200

    fix: handle empty services

--- src/components/ServiceCanvas.svelte
+++ src/components/ServiceCanvas.svelte
@@ -1,10 +1,11 @@
 <script>
   export let services;
+
   let width = 500;
   let height = 1400;
 
   function connectionPath(from, to) {
-    return `M60 0H${from.rx}V${(to.owner.y + to.y) - (from.owner.y + from.y)}H60`;
+    return `M60 0H${from.rx}V${to.owner.y + to.y - (from.owner.y + from.y)}H60`;
   }
 
   function clickService(event) {
@@ -77,39 +78,43 @@
 
 <svg viewbox="0 0 {width} {height}">
   <g class="services">
-    {#each Object.values(services.services) as service}
-      <g class="service" transform="translate({service.x},{service.y})" on:click={clickService}>
-        <rect
-          class={service.state}
-          x="0"
-          y="0"
-          width={service.w}
-          height={service.h} />
-        <text x="8" y="22">{service.name} ({service.type})</text>
-        {#each Object.values(service.endpoints) as endpoint}
-          <g
-            class={endpoint.isOpen ? 'endpoint open' : 'endpoint'}
-            transform="translate({endpoint.x - 60},{endpoint.y})">
+    {#if services.services}
+      {#each Object.values(services.services) as service}
+        <g
+          class="service"
+          transform="translate({service.x},{service.y})"
+          on:click={clickService}>
+          <rect
+            class={service.state}
+            x="0"
+            y="0"
+            width={service.w}
+            height={service.h} />
+          <text x="8" y="22">{service.name} ({service.type})</text>
+          {#each Object.values(service.endpoints) as endpoint}
+            <g
+              class={endpoint.isOpen ? 'endpoint open' : 'endpoint'}
+              transform="translate({endpoint.x - 60},{endpoint.y})">
+              <text x={52} y={3}>{endpoint.name}</text>
+              {#if endpoint.isIn}
+                <rect x="55" y="-5" width="10" height="10" />
 
-            <text x={52} y={3}>{endpoint.name}</text>
-            {#if endpoint.isIn}
-              <rect x="55" y="-5" width="10" height="10" />
+                {#each [...endpoint.connections()] as connected}
+                  <path
+                    class="connection"
+                    d={connectionPath(endpoint, connected)} />
+                {/each}
+              {:else}
+                <circle cx="60" cy="0" r="5" />
+              {/if}
 
-              {#each [...endpoint.connections()] as connected}
-                <path
-                  class="connection"
-                  d={connectionPath(endpoint, connected)} />
+              {#each endpoint.interceptors as interceptor}
+                <circle class="interceptor" cx="72" cy="0" r="5" />
               {/each}
-            {:else}
-              <circle cx="60" cy="0" r="5" />
-            {/if}
-
-            {#each endpoint.interceptors as interceptor}
-              <circle class="interceptor" cx="72" cy="0" r="5" />
-            {/each}
-          </g>
-        {/each}
-      </g>
-    {/each}
+            </g>
+          {/each}
+        </g>
+      {/each}
+    {/if}
   </g>
 </svg>
