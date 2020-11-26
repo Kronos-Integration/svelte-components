5e4f1377a011de8cd209a6806020335ad8af1f1e
Markus Felten <Markus.Felten@gmx.de>
2020-11-26 19:24:32 +0100

    chore: add postcss

--- tests/app/rollup.config.mjs
+++ tests/app/rollup.config.mjs
@@ -1,11 +1,9 @@
-import postcss from "rollup-plugin-postcss";
-
 import virtual from "@rollup/plugin-virtual";
-
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import dev from "rollup-plugin-dev";
 import svelte from "rollup-plugin-svelte";
+import postcss from "rollup-plugin-postcss";
 
 const basedir = "tests/app";
 const port = 5000;
@@ -17,18 +15,25 @@ export default {
     format: "esm",
     file: `${basedir}/public/bundle.main.mjs`
   },
-  plugins: [dev({
-    port,
-    dirs: [`${basedir}/public`],
-    spa: `${basedir}/public/index.html`,
-    basePath: `/components/kronos-integration/svelte-components/${basedir}`
-  }), svelte(), commonjs(), resolve({
-    browser: true,
-    dedupe: importee =>
-      importee === "svelte" || importee.startsWith("svelte/")
-  }), virtual({
-    "node-fetch": "export default fetch",
-    stream: "export class Readable {}",
-    buffer: "export class Buffer {}"
-  }), postcss()]
+  plugins: [
+    virtual({
+      "node-fetch": "export default fetch",
+      stream: "export class Readable {}",
+      buffer: "export class Buffer {}"
+    }),
+    svelte(),
+    postcss(),
+    commonjs(),
+    resolve({
+      browser: true,
+      dedupe: importee =>
+        importee === "svelte" || importee.startsWith("svelte/")
+    }),
+    dev({
+      port,
+      dirs: [`${basedir}/public`],
+      spa: `${basedir}/public/index.html`,
+      basePath: `/components/kronos-integration/svelte-components/${basedir}`
+    })
+  ]
 };
