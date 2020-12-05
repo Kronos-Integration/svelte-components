3e497f5bfce60ddd49313054cb482b785f5fa881
Markus Felten <Markus.Felten@gmx.de>
2020-12-05 13:34:07 +0100

    fix: separate Service.svelte

---
+++ src/components/Service.svelte
@@ -0,0 +1,61 @@
+<script>
+  import Endpoint from "./Endpoint.svelte";
+
+  export let service;
+
+  function clickService(event) {}
+
+  function dragStartService(event) {
+    console.log(event);
+  }
+</script>
+
+<style>
+  .service {
+    pointer-events: auto;
+  }
+
+  .service rect {
+    stroke: none;
+    opacity: 0.35;
+  }
+
+  .open {
+    fill: green;
+    stroke: green;
+  }
+
+  .running {
+    fill: green;
+  }
+  .starting {
+    fill: lightgreen;
+  }
+  .stopped {
+    fill: gray;
+  }
+  .stopping {
+    fill: lightgray;
+  }
+  .failed {
+    fill: red;
+  }
+</style>
+
+<g
+  class="service"
+  transform="translate({service.x},{service.y})"
+  on:click={clickService}
+  on:dragstart={dragStartService}>
+  <rect
+    class={service.state}
+    x="0"
+    y="0"
+    width={service.w}
+    height={service.h}
+    rx="5" />
+  <text x="8" y="22">{service.name} ({service.type})</text>
+  {#each Object.values(service.endpoints) as endpoint}
+    <Endpoint {endpoint} />
+  {/each}
+</g>
--- src/components/ServiceCanvas.svelte
+++ src/components/ServiceCanvas.svelte
@@ -1,49 +1,12 @@
 <script>
-  import Endpoint from "./Endpoint.svelte";
+  import Service from "./Service.svelte";
 
   export let services;
-
-  function clickService(event) {
-  }
-
-  function dragStartService(event) {
-    console.log(event);
-  }
 </script>
 
 <style>
   svg {
     pointer-events: none;
-  }
-
-  .service {
-    pointer-events: auto;
-  }
-
-  .service rect {
-    stroke: none;
-    opacity: 0.35;
-  }
-
-  .open {
-    fill: green;
-    stroke: green;
-  }
-
-  .running {
-    fill: green;
-  }
-  .starting {
-    fill: lightgreen;
-  }
-  .stopped {
-    fill: gray;
-  }
-  .stopping {
-    fill: lightgray;
-  }
-  .failed {
-    fill: red;
   }
 </style>
 
@@ -77,23 +40,7 @@
   <g class="services">
     {#if services.services}
       {#each Object.values(services.services) as service}
-        <g
-          class="service"
-          transform="translate({service.x},{service.y})"
-          on:click={clickService}
-          on:dragstart={dragStartService}>
-          <rect
-            class={service.state}
-            x="0"
-            y="0"
-            width={service.w}
-            height={service.h}
-            rx="5" />
-          <text x="8" y="22">{service.name} ({service.type})</text>
-          {#each Object.values(service.endpoints) as endpoint}
-           <Endpoint {endpoint}/>
-          {/each}
-        </g>
+        <Service {service} />
       {/each}
     {/if}
   </g>
